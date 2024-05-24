4ae9cfd2fde8f9c293e25868b55911b3214aca30
Michael Knych <mmknych@yahoo.com>
2024-05-24 17:33:39 -0500

    Initial Commit

---
+++ Charts Demo 4/Charts_Demo_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_Demo_4App.swift
+//  Charts Demo 4
+//
+//  Created by Michael Knych on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Charts_Demo_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
